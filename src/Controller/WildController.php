5f3607cf418c135e0bc6742dc7b0a47bef39528d
Manuel <leger.manuel@hotmail.fr>
2020-04-30 14:14:05 +0200

    Symfony 8 récuperer des données stockées avec doctrine

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,19 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild")
+     * @Route("/wild", name="wild_index")
      */
     public function index()
     {
-        return $this->render('wild/index.html.twig', [
-            'controller_name' => 'WildController',
-        ]);
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No program found in program\'s table.'
+            );
+        }
+
+        return $this->render(
+            'wild/index.html.twig',
+            ['programs' => $programs]
+        );
     }
     /**
      * @Route("/wild/show/{slug<^[a-z0-9-]+$>}",defaults={"slug"= null}, name="wild_show")
@@ -25,9 +39,57 @@ class WildController extends AbstractController
         {
             throw $this->createNotFoundException("Aucune série sélectionnée, veuillez choisir une série");
         }
-        $slug = str_replace("-"," ",$slug);
-        $slug = ucwords($slug);
-        return $this->render('wild/show.html.twig',[ 'slug' => $slug]);
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with '.$slug.' title, found in program\'s table.'
+            );
+        }
+
+        return $this->render('wild/show.html.twig', [
+            'program' => $program,
+            'slug'  => $slug,
+        ]);
+    }
+    /**
+     * @Route("wild/category/{categoryName}", name="show_category").
+     */
+    public function showByCategory(string $categoryName)
+    {
+
+        $repositoryCategory = $this->getDoctrine()->getRepository(Category::class);
+
+        $category_str = $repositoryCategory->findOneBy(['name' => $categoryName]);
+
+        if(!$category_str)
+        {
+            throw $this->createNotFoundException('Aucune catégorie trouvé avec'. $category_str);
+        }
+
+        $idCategory = $category_str->getId();
+
+        $repositoryProgram = $this->getDoctrine()
+            ->getRepository(Program::class);
+
+        $Programs = $repositoryProgram->findBy(['category' => $idCategory],
+            ['id' => 'desc'],
+            3,
+            0);
+
+        return $this->render('wild/category.html.twig', ['Programs'=> $Programs]);
+
+
+
 
     }
+
+
 }
+
